9e2d568d2739d7cfdfd899a3d0aad04a2c2c4629
azyobuzin <azyobuzin@users.sourceforge.jp>
2011-11-12 22:20:30 +0900

    Initial commit

---
+++ YacqPlugin/EntryPoint.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Acuerdo.Plugin;
+using Inscribe.Storage;
+using XSpect.Yacq;
+
+namespace YacqPlugin
+{
+    [Export(typeof(IPlugin))]
+    public class EntryPoint : IPlugin
+    {
+        public string Name
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly()
+                    .GetCustomAttributes(false)
+                    .OfType<AssemblyTitleAttribute>()
+                    .Select(a => a.Title)
+                    .FirstOrDefault();
+            }
+        }
+
+        public double Version
+        {
+            get
+            {
+                var version = Assembly.GetExecutingAssembly().GetName().Version;
+                return double.Parse(version.Major + "." + version.Minor);
+            }
+        }
+
+        public void Loaded()
+        {
+            try
+            {
+                Directory.EnumerateFiles(
+                    Path.Combine(
+                        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                        "yacq_lib"
+                    ),
+                    "*.yacq",
+                    SearchOption.TopDirectoryOnly
+                )
+                .Select(file => File.ReadAllText(file))
+                .ForEach(code =>
+                {
+                    try
+                    {
+                        YacqServices.ParseAll(code)
+                            .Select(exp => Expression.Lambda(exp).Compile())
+                            .ForEach(dlg => dlg.DynamicInvoke());
+                    }
+                    catch (Exception ex)
+                    {
+                        ExceptionStorage.Register(ex, ExceptionCategory.PluginError);
+                    }
+                });
+            }
+            catch { }
+        }
+
+        public IConfigurator ConfigurationInterface
+        {
+            get { return null; }
+        }
+    }
+}
---
+++ YacqPlugin/Readme.txt
@@ -0,0 +1,12 @@
+﻿YACQ Plugin
+作者：@azyobuzin
+動作環境：Krile 2.2.1くらい
+
+YACQスクリプトを実行するKrile用プラグインです。
+IPlugin.Loadedされたときにコンパイル・実行します。
+
+★使い方
+	1. pluginsフォルダにdll類を突っ込みます
+	2. pluginsフォルダに「yacq_lib」というフォルダを作って
+	   スクリプト（.yacqファイル）を投げ込みます
+	3. Krileを起動すれば動き出します
